1419b72d5b66f9f8f72310effc7983864d4dc496
Stieilijh <saitushar02@gmail.com>
2022-11-09 10:56:26 +0530

    clean up

--- app/screens/ShareFileScreen.js
+++ app/screens/ShareFileScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import React, { useState } from "react";
+import React from "react";
 import * as FileSystem from "expo-file-system";
 import * as Sharing from "expo-sharing";
 
